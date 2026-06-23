f87e0385b6e6104c45fe982b4200e51b5e002bd3
GregoryAndroidDeveloper <belyakovgrisha2000@yandex.ru>
2026-06-23 13:46:14 +0800

    first practical work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название автомобиля под номером " + i + ":");
+            String name = scanner.next();
+
+            int speed;
+
+            while (true) {
+                System.out.println("Введите скорость автомобиля под номером " + i + ":");
+                speed = scanner.nextInt();
+
+                if (speed > 0 && speed <= 250) {
+                    break;
+                } else {
+                    System.out.println("Неверная скорость. Попробуйте еще раз");
+                }
+            }
+
+            Car car = new Car(name, speed);
+            race.defineLeader(car);
+        }
+        System.out.println("Победитель гонки: " + race.leader + "!");
     }
 }
